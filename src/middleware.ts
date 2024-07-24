b610bbd1bcdf64a5c1853c352dc93ca17c744e0d
Carlos Chao <chao@factory.generalsoftwareinc.com>
2024-07-24 15:56:39 -0400

    feat: add /api to public routes

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,6 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isPublicRoute = createRouteMatcher(["/sign-in(.*)", "/sign-up(.*)"]);
+const isPublicRoute = createRouteMatcher(["/sign-in(.*)", "/sign-up(.*)", "/api/(.*)"]);
 
 export default clerkMiddleware(
   (auth, req) => {
