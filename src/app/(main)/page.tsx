7f2917f6d955130683397779f2f717b09befaf1f
Carlos Chao <chao@factory.generalsoftwareinc.com>
2024-08-21 10:18:03 -0400

    feat: update the main page to catch getUserError

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,4 +1,5 @@
 import { getUserTasks } from "@/lib/server/services/task";
+import { CalendarTask } from "@/lib/server/types";
 import CalendarContainer from "@/modules/calendar/containers/calendar";
 import { auth } from "@clerk/nextjs/server";
 
@@ -7,9 +8,12 @@ export default async function Home() {
   if (!userId) {
     throw new Error("User id is required");
   }
-
-  const tasks = await getUserTasks(userId);
-  
+  let tasks: CalendarTask[] = [];
+  try {
+    tasks = await getUserTasks(userId);
+  } catch (e) {
+    console.log(e);
+  }
   return (
     <div className="h-full w-full overflow-y-auto">
       <CalendarContainer tasks={tasks} />
